4a4c7435d8d58cb60b0c3580ce714799211d67dd
KarishmaGR <Karishmasvryadav@gmail.com>
2023-05-02 13:54:08 +0530

    footer added

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import {AiOutlineGithub} from "react-icons/ai";
+import {BsInstagram,BsYoutube} from "react-icons/bs"
+import "./home.scss";
+import { Link } from 'react-router-dom';
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <p>Made by Govind</p>
+      <div className="icons">
+      <Link to={"https://github.com/Govind312?tab=repositories"} target='blank'>
+        <AiOutlineGithub color='white'/></Link>
+      <Link to={"https://www.instagram.com/9ovindyadav"} target='blank'> 
+      <BsInstagram color='orange'/>
+      </Link>
+      <Link to={"https://www.youtube.com/@9ovindyadav"} target='blank'>
+      <BsYoutube color='red'/>
+      </Link>
+      </div>
+    </div>
   )
 }
